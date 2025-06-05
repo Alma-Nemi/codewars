e9ebf9d10ce9af8ec8a7af952c619f5911fdf766
Alma-Nemi <alma.nemi@gmail.com>
2025-06-06 00:49:25 +0400

    feat: add tests for Even or Odd (8 kyu)

--- 8-kyu/even-or-odd.js
+++ 8-kyu/even-or-odd.js
@@ -4,10 +4,17 @@
  * @returns {string} "Even" if the number is even, "Odd" if the number is odd.
  */
 function evenOrOdd(number) {
-     if (number % 2 === 0) {
-          return "Even";
-     }
-     else {
-          return "Odd";
-     }
+    if (number % 2 === 0) {
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
 }
+
+// Tests from Codewars
+console.assert(evenOrOdd(2) === "Even", "2 should be Even");
+console.assert(evenOrOdd(7) === "Odd", "7 should be Odd");
+console.assert(evenOrOdd(-42) === "Even", "-42 should be Even");
+console.assert(evenOrOdd(-7) === "Odd", "-7 should be Odd");
+console.assert(evenOrOdd(0) === "Even", "0 should be Even");
