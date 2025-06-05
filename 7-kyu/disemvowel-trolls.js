e327b69bf7549661c5e93bc894ca40bb3c6dc399
Alma-Nemi <alma.nemi@gmail.com>
2025-06-06 00:57:12 +0400

    feat: add tests for Disemvowel Trolls (7 kyu)

--- 7-kyu/disemvowel-trolls.js
+++ 7-kyu/disemvowel-trolls.js
@@ -1,8 +1,16 @@
 /**
  * Removes all vowels from the input string.
- * @param {string} str - The input string to process.
- * @returns {string} The input string with all vowels removed.
+ * @param {string} str - The input string from which vowels will be removed.
+ * @returns {string} The input string with all vowels removed (both lowercase and uppercase).
  */
 function disemvowel(str) {
   return str.replace(/[aeiouAEIOU]/g, '');
 }
+
+// Tests from Codewars
+console.assert(disemvowel("This website is for losers LOL!") === "Ths wbst s fr lsrs LL!", 
+  "Vowels should be removed");
+console.assert(disemvowel("No offense but,\nYour writing is among the worst I've ever read") === 
+  "N ffns bt,\nYr wrtng s mng th wrst 'v vr rd", "Vowels should be removed with newlines");
+console.assert(disemvowel("What are you, a communist?") === "Wht r y,  cmmnst?", 
+  "Vowels and punctuation should be handled");
